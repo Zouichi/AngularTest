7481addbba8904ba7afbe32027cfb37a35fb399c
Boiteux Rémi <boiteux.remi@gmail.com>
2017-08-29 11:21:29 +0200

    App modified

--- app/myApp.js
+++ app/myApp.js
@@ -1,1 +1,32 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(["$routeProvider", "$locationProvider",
+   function($routeProvider, $locationProvider) {
+       
+       $routeProvider
+       .when('/',{
+       		templateUrl : 'app/views/vue1.html',
+       		controller : "myCtrl"
+       })
+
+       .when('/page2',{
+	       	templateUrl : 'app/views/vue2.html',
+	       	controller : "myCtrl2"
+	       })
+       .otherwise({
+       		redirectTo: '/'
+       	
+       });
+       
+       // Système de routage
+       // $routeProvider
+       // .when(‘/home’, {
+       //     templateUrl: ‘partials/home.html’,
+       //     controller: ‘homeCtrl’
+       // })
+       // .when(‘/contact’, {
+       //     templateUrl: ‘partials/contact.html’,
+       //     controller: ‘contactCtrl’
+       // });
+   }
+]);
