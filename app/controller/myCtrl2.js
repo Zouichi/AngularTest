e840ac91aed626b437fbd10653dad3651ae9ddfe
Boiteux Rémi <boiteux.remi@gmail.com>
2017-08-30 16:03:55 +0200

    app modified

--- app/controller/myCtrl2.js
+++ app/controller/myCtrl2.js
@@ -1,3 +1,7 @@
-app.controller("myCtrl2", function($scope) {
+app.controller("myCtrl2", function($scope, $http,liste) {
     // $scope.monTableau = [{nom : "casagrande", prenom:"leo"},{nom : "vador", prenom:"dark"}];
+    $scope.maliste = liste;
+    console.log(liste);
+    console.log($scope.maliste);
+
 });
--- app/myApp.js
+++ app/myApp.js
@@ -1,32 +1,40 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module("myApp", ['ngRoute', 'ngResource']);
 
-app.config(["$routeProvider", "$locationProvider",
-   function($routeProvider, $locationProvider) {
-       
-       $routeProvider
-       .when('/',{
-       		templateUrl : 'app/views/vue1.html',
-       		controller : "myCtrl"
-       })
+app.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
 
-       .when('/page2',{
-	       	templateUrl : 'app/views/vue2.html',
-	       	controller : "myCtrl2"
-	       })
-       .otherwise({
-       		redirectTo: '/'
-       	
-       });
-       
-       // Système de routage
-       // $routeProvider
-       // .when(‘/home’, {
-       //     templateUrl: ‘partials/home.html’,
-       //     controller: ‘homeCtrl’
-       // })
-       // .when(‘/contact’, {
-       //     templateUrl: ‘partials/contact.html’,
-       //     controller: ‘contactCtrl’
-       // });
-   }
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/views/vue1.html',
+                controller: "myCtrl"
+            })
+
+        .when("/page2", {
+                templateUrl: 'app/views/vue2.html',
+                controller: "myCtrl2",
+                resolve: {
+                    liste : function(eleveFactory){
+                        // var promise = $q.defer();
+
+                        // eleveFactory.query().$promise.then(function(data){
+                        //     console.log(data);
+                        //     promise.resolve(data);
+                        // });
+                        // return promise.promise
+
+                        return eleveFactory.query()
+
+
+                    }
+                }
+            })
+            .when("/eleve/:id", {
+                templateUrl: 'app/views/profil.html',
+                controller: "profil.ctrl"
+            })
+            .otherwise({
+                redirectTo: '/',
+
+            });
+    }
 ]);
